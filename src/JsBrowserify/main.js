38cbcd71c3a8124387cefb64e6565f92334797b8
naach <napat_pat3@hotmail.com>
2021-01-18 02:39:10 +0700

    feat: connect port to get cashinfo

--- src/JsBrowserify/main.js
+++ src/JsBrowserify/main.js
@@ -2,9 +2,11 @@ const randomBytes = require('randombytes')
 const base64url = require('base64url')
 // Initial data passed to Elm (should match `Flags` defined in `Shared.elm`)
 // https://guide.elm-lang.org/interop/flags.html
+// DO NOT CHANGE:REFER Index.elm
 const flags = null
 
 // Start our Elm application
+// DO NOT CHANGE:REFER Index.elm
 const app = Elm.Main.init({ flags: flags })
 
 // Ports go here
@@ -44,37 +46,83 @@ switch (NETWORK) {
     regtest = true
 }
 
+// FUNCTION
 function newBase64RootSeed () {
   const entropy16 = randomBytes(16)
   return entropy16.toString('base64')
 }
 
-function decodedBase64 () {
+function decodedBase64 (base64RootSeed) {
   return Buffer.from(base64RootSeed.toString('base64'), 'base64')
 }
 
+function mnemonicFromRootSeed (rootSeed) {
+  return bchjs.Mnemonic.fromEntropy(rootSeed, bchjs.Mnemonic.wordLists()[lang])
+}
+
+function getChildNode0 (rootSeed) {
+  const mnemonic = mnemonicFromRootSeed(rootSeed)
+  // console.log('MNEMONIC', mnemonic)
+  const seedBuffer = bchjs.Mnemonic.toSeed(mnemonic)
+  // console.log('SEEDBUFFER', seedBuffer)
+  const masterHDNode = bchjs.HDNode.fromSeed(rootSeed, NETWORK)
+  // console.log('MASTERHDNODE', masterHDNode)
+  const childNode0 = masterHDNode.derivePath(`m/44'/145'/0'/0/0`)
+  // console.log('CHILDNODE0', childNode0)
+  return childNode0
+}
+
+function getCashAddress0(childNode0) {
+  const cashAddress0 = bchjs.HDNode.toCashAddress(childNode0, regtest)
+  // console.log('CASHADDRESS', cashAddress0)
+  return cashAddress0
+}
+
+async function getBalance (cashAddress) {
+  try {
+    const balance = await bchjs.Electrumx.balance(cashAddress)
+    return balance
+  } catch (err) {
+    console.error('Error in getBalance: ', err)
+    console.log(`Error message: ${err.message}`)
+    throw err
+  }
+}
+
+// PORT
+app.ports.gettingCashInfo.subscribe(async function (encodedBase64RootSeed) {
+    // console.log('[elm-to-js]', encodedBase64RootSeed)
+    const decodedBase64RootSeed = base64url.toBase64(encodedBase64RootSeed)
+    const rootSeed = decodedBase64 (decodedBase64RootSeed)
+    const childNode0 = getChildNode0 (rootSeed)
+    // console.log('CHILDNODE0', childNode0)
+
+    const cashAddress0 = getCashAddress0(childNode0)
+    // console.log('CASHADDRESS', cashAddress0)
+    const balance = await getBalance(cashAddress0)
+    // console.log('BALANCE', balance.balance)
+    app.ports.gotCashInfo.send(
+      cashAddress0
+    );
+})
 
 
-const base64RootSeed = newBase64RootSeed()
-console.log('BASE64ROOTSEED', base64RootSeed)
-const rootSeed = Buffer.from(base64RootSeed.toString('base64'), 'base64')
-console.log('ROOTSEED', rootSeed)
-const mnemonic = bchjs.Mnemonic.fromEntropy(rootSeed, bchjs.Mnemonic.wordLists()[lang])
-console.log('MNEMONIC', mnemonic)
-const seedBuffer = bchjs.Mnemonic.toSeed(mnemonic)
-console.log('SEEDBUFFER', seedBuffer)
-const masterHDNode = bchjs.HDNode.fromSeed(rootSeed, NETWORK)
-// console.log('MASTERHDNODE', masterHDNode)
-const childNode0 = masterHDNode.derivePath(`m/44'/145'/0'/0/0`)
-console.log('CHILDNODE0', childNode0)
-const cashAddress0 = bchjs.HDNode.toCashAddress(childNode0, regtest)
-console.log('CASHADDRESS', cashAddress0)
+// const rootSeed = decodedBase64(base64RootSeed)
+// console.log('ROOTSEED', rootSeed)
 
 
+// const mnemonic = mnemonicFromRootSeed(rootSeed)
+// console.log('MNEMONIC', mnemonic)
+// const seedBuffer = bchjs.Mnemonic.toSeed(mnemonic)
+// console.log('SEEDBUFFER', seedBuffer)
+// const masterHDNode = bchjs.HDNode.fromSeed(rootSeed, NETWORK)
+// // console.log('MASTERHDNODE', masterHDNode)
 
-const encodedBase64RootSeed = base64url.fromBase64(rootSeed)
-console.log('ENCODEDBASE64ROOTSEED', encodedBase64RootSeed)
-const decodedBase64RootSeed = base64url.toBase64(encodedBase64RootSeed)
-console.log('DECODEDBASE64ROOTSEED', decodedBase64RootSeed)
-rootSeed == decodedBase64RootSeed
-console.log('ROOTSEED == DECODEDBASE64ROOTSEED', rootSeed == decodedBase64RootSeed)
+//
+//
+//
+// const encodedBase64RootSeed = base64url.fromBase64(base64RootSeed)
+// console.log('ENCODEDBASE64ROOTSEED', encodedBase64RootSeed)
+// const decodedBase64RootSeed = base64url.toBase64(encodedBase64RootSeed)
+// console.log('DECODEDBASE64ROOTSEED', decodedBase64RootSeed)
+// console.log('ROOTSEED == DECODEDBASE64ROOTSEED', base64RootSeed == decodedBase64RootSeed)
--- src/Pages/Bch.elm
+++ src/Pages/Bch.elm
@@ -1,46 +1,110 @@
-module Pages.Bch exposing (Model, Msg, Params, page)
+port module Pages.Bch exposing (Model, Msg, Params, page)
 
 import Dict exposing (Dict)
 import Element exposing (..)
 import Element.Font as Font
 import Html
 import Html.Attributes
+import Shared
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
 import Spa.Url as Url exposing (Url)
 
 
-page : Page Params Model Msg
-page =
-    Page.static
-        { view = view
-        }
+
+-- PORT
 
 
-type alias Model =
-    Url Params
+port gettingCashInfo : String -> Cmd msg
 
 
-type alias Msg =
-    Never
+port gotCashInfo : (String -> msg) -> Sub msg
 
 
 
--- VIEW
+-- INIT
 
 
 type alias Params =
     ()
 
 
-view : Url Params -> Document Msg
-view url =
+type alias Model =
+    Url Params
+
+
+init : Shared.Model -> Url Params -> ( Model, Cmd Msg )
+init shared url =
     let
         wallet =
             Dict.get "w" url.query
 
         _ =
-            Debug.log "debug" wallet
+            Debug.log "debug" shared
+    in
+    case Dict.get "w" url.query of
+        Just query ->
+            ( url, gettingCashInfo query )
+
+        Nothing ->
+            ( url, Cmd.none )
+
+
+page : Page Params Model Msg
+page =
+    Page.application
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        , save = save
+        , load = load
+        }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = MsgCashInfo String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MsgCashInfo balance ->
+            let
+                _ =
+                    Debug.log "debug" balance
+            in
+            ( model, Cmd.none )
+
+
+save : Model -> Shared.Model -> Shared.Model
+save model shared =
+    shared
+
+
+load : Shared.Model -> Model -> ( Model, Cmd Msg )
+load shared model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    gotCashInfo MsgCashInfo
+
+
+
+-- VIEW
+
+
+view : Model -> Document Msg
+view model =
+    let
+        _ =
+            Debug.log "debug" model
     in
     { title = "PageA"
     , body =
