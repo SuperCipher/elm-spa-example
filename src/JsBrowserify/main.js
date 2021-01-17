7624fcfcd704edc53153a0be62750f9760adbece
naach <napat_pat3@hotmail.com>
2021-01-17 16:02:19 +0700

    feat: add browserify js to incorperate npm package

--- src/JsBrowserify/main.js
+++ src/JsBrowserify/main.js
@@ -7,3 +7,9 @@ const app = Elm.Main.init({ flags: flags })
 
 // Ports go here
 // https://guide.elm-lang.org/interop/ports.html
+
+
+const randomBytes = require('randombytes')
+
+const entropy16 = randomBytes(16)
+console.log('ENTROPY16', entropy16)
